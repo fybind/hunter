dd6cbc7095ea49b05c731e9ab47c1bd2cdd30e1e
fybind <musikkup@gmail.com>
2025-01-23 03:28:06 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,6 +12,7 @@ from config import (
     CHANNEL_ID,
     FORCE_SUB_CHANNEL,
     FORCE_SUB_GROUP,
+    FORCE_SUB_GROUP1,
     LOGGER,
     OWNER,
     TG_BOT_TOKEN,
@@ -94,6 +95,31 @@ class Bot(Client):
                     "Bot Berhenti. Gabung Group https://t.me/SharingUserbot untuk Bantuan"
                 )
                 sys.exit()
+
+                if FORCE_SUB_GROUP1:
+            try:
+                info = await self.get_chat(FORCE_SUB_GROUP1)
+                link = info.invite_link
+                if not link:
+                    await self.export_chat_invite_link(FORCE_SUB_GROUP1)
+                    link = info.invite_link
+                self.invitelink3 = link
+                self.LOGGER(__name__).info(
+                    f"FORCE_SUB_GROUP1 detected!\n┌ Title: {info.title}\n└ Chat ID: {info.id}\n——"
+                )
+            except Exception as a:
+                self.LOGGER(__name__).warning(a)
+                self.LOGGER(__name__).warning(
+                    "Bot tidak dapat Mengambil link invite dari FORCE_SUB_GROUP1!"
+                )
+                self.LOGGER(__name__).warning(
+                    f"Pastikan @{self.username} adalah admin di Group Tersebut, Chat ID F-Subs Group Saat Ini: {FORCE_SUB_GROUP}"
+                )
+                self.LOGGER(__name__).info(
+                    "Bot Berhenti. Gabung Group https://t.me/SharingUserbot untuk Bantuan"
+                )
+                sys.exit()
+
 
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
