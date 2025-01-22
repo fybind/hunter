c2c0abfd1b5efe1be4cfb19f49db4ecba494dba1
fybind <musikkup@gmail.com>
2025-01-23 04:31:07 +0700

    Update config.py

--- config.py
+++ config.py
@@ -38,6 +38,7 @@ DB_URI = os.environ.get("DATABASE_URL", "postgres://oqkcmrac:ELbsJkKImZ8F6S2QYrp
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "0"))
+FORCE_SUB_GROUP1 = int(os.environ.get("FORCE_SUB_GROUP1", "0"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
